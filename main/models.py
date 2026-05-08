555d3395c17afd633733e7ab4086960f56ab17fe
Твоё Имя <kaorukoit@gmail.com>
2026-05-08 13:59:19 +0600

    update

--- main/models.py
+++ main/models.py
@@ -5,20 +5,18 @@ from django.contrib.auth.models import User
 
 # Модель темы (категория обучения)
 class Topic(models.Model):
-    # Название темы (например: "Криптография")
+    # Название темы
     title = models.CharField(max_length=200, verbose_name='Название темы')
 
     # Краткое описание темы
     description = models.TextField(verbose_name='Краткое описание')
 
     def __str__(self):
-        # Как объект будет отображаться в админке
         return self.title
 
 
 # Модель урока (теория)
 class Lesson(models.Model):
-    # Связь с темой (одна тема → много уроков)
     topic = models.ForeignKey(
         Topic,
         on_delete=models.CASCADE,
@@ -28,8 +26,6 @@ class Lesson(models.Model):
 
     # Название урока
     title = models.CharField(max_length=200, verbose_name='Название урока')
-
-    # Основной текст теории
     content = models.TextField(verbose_name='Теория')
 
     def __str__(self):
@@ -38,7 +34,6 @@ class Lesson(models.Model):
 
 # Модель теста
 class Test(models.Model):
-    # Связь с темой (одна тема → один или несколько тестов)
     topic = models.ForeignKey(
         Topic,
         on_delete=models.CASCADE,
@@ -56,7 +51,6 @@ class Test(models.Model):
 
 # Модель вопроса
 class Question(models.Model):
-    # Связь с тестом (один тест → много вопросов)
     test = models.ForeignKey(
         Test,
         on_delete=models.CASCADE,
@@ -68,14 +62,12 @@ class Question(models.Model):
     text = models.TextField(verbose_name='Текст вопроса')
 
     def __str__(self):
-        # Ограничиваем вывод, чтобы не было слишком длинного текста
         return self.text[:50]
 
 
 
 # Модель ответа
 class Answer(models.Model):
-    # Связь с вопросом (один вопрос → много ответов)
     question = models.ForeignKey(
         Question,
         on_delete=models.CASCADE,
@@ -95,14 +87,14 @@ class Answer(models.Model):
 
 # Модель результата теста
 class Result(models.Model):
-    # Пользователь, который прошёл тест
+    # Пользователь который прошёл тест
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='results',
         verbose_name='Пользователь'
     )
-    # Тест, который был пройден
+
     test = models.ForeignKey(
         Test,
         on_delete=models.CASCADE,
@@ -111,7 +103,6 @@ class Result(models.Model):
     )
     # Количество набранных баллов
     score = models.IntegerField(verbose_name='Количество баллов')
-    # Дата прохождения теста
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата прохождения')
 
     def __str__(self):
@@ -120,7 +111,6 @@ class Result(models.Model):
 
 # Модель профиля пользователя
 class Profile(models.Model):
-    # Связь один к одному с пользователем
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
